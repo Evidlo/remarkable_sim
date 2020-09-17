bc6c3423e018ef204abb315d38df71a9acd5591a
evan <evan@evanw.org>
2020-09-17 00:34:39 -0500

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
     name='remarkable-sim',
-    version='1.0.0',
+    version='1.0.2',
     packages=['remarkable_sim'],
     author="Evan Widloski",
     author_email="evan@evanw.org",
